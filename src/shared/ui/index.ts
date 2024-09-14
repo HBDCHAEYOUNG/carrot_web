b3afda3aca2a81e04e4f6304afd4ba7f7252acca
Whyukim <devhyukim@gmail.com>
2024-09-15 02:25:24 +0900

    🎨 Finish

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,6 +1,5 @@
 export * from './_shardcn/button'
 export * from './_shardcn/form'
-export * from './_shardcn/input'
 export * from './_shardcn/label'
 export * from './_shardcn/menubar'
 export * from './form/form'
