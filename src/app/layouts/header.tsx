bdd0b5044fafd75e298d7618bc6a60fdec1e1067
Whyukim <devhyukim@gmail.com>
2024-09-14 21:33:22 +0900

    💚 환경설정 완료

--- src/app/layouts/header.tsx
+++ src/app/layouts/header.tsx
@@ -1,46 +1,41 @@
-import Logo from "@images/carrot.svg?react";
-import {
-  MenubarContent,
-  MenubarItem,
-  MenubarMenu,
-  MenubarSeparator,
-  MenubarTrigger,
-} from "@radix-ui/react-menubar";
-import { Menubar } from "@ui/_shardcn/menubar";
-import { MdOutlineKeyboardArrowUp } from "react-icons/md";
-import { CiBellOn, CiSearch } from "react-icons/ci";
-import useSearchStore from "@store/searchStore";
+// import Logo from "@images/carrot.svg?react";
+import { MenubarContent, MenubarItem, MenubarMenu, MenubarSeparator, MenubarTrigger } from '@radix-ui/react-menubar'
+import useSearchStore from '@store/searchStore'
+import { CiBellOn, CiSearch } from 'react-icons/ci'
+import { MdOutlineKeyboardArrowUp } from 'react-icons/md'
+
+import { Menubar } from '@ui/_shardcn/menubar'
 
 function Header() {
-  const region = ["읍내동", "동천동", "구암동"];
-  const { searchMode, toggleSearchMode } = useSearchStore();
+	const region = ['읍내동', '동천동', '구암동']
+	const { searchMode, toggleSearchMode } = useSearchStore()
 
-  return (
-    <header className="w-full flex items-center justify-between">
-      <Menubar>
-        <MenubarMenu>
-          <MenubarTrigger>
-            내 동네 <MdOutlineKeyboardArrowUp />
-          </MenubarTrigger>
-          <MenubarContent>
-            <MenubarSeparator />
-            {region.map((items) => (
-              <MenubarItem>{items}</MenubarItem>
-            ))}
-            <MenubarItem>내 동네 설정</MenubarItem>
-          </MenubarContent>
-        </MenubarMenu>
-      </Menubar>
+	return (
+		<header className="flex w-full items-center justify-between">
+			<Menubar>
+				<MenubarMenu>
+					<MenubarTrigger>
+						내 동네 <MdOutlineKeyboardArrowUp />
+					</MenubarTrigger>
+					<MenubarContent>
+						<MenubarSeparator />
+						{region.map((items) => (
+							<MenubarItem>{items}</MenubarItem>
+						))}
+						<MenubarItem>내 동네 설정</MenubarItem>
+					</MenubarContent>
+				</MenubarMenu>
+			</Menubar>
 
-      <Logo />
+			{/* <Logo /> */}
 
-      <div>
-        <CiSearch className="h-6 w-6" onClick={toggleSearchMode} />
-        {searchMode && <p>안녕</p>}
-        <CiBellOn className="h-6 w-6" />
-      </div>
-    </header>
-  );
+			<div>
+				<CiSearch className="h-6 w-6" onClick={toggleSearchMode} />
+				{searchMode && <p>안녕</p>}
+				<CiBellOn className="h-6 w-6" />
+			</div>
+		</header>
+	)
 }
 
-export default Header;
+export default Header
--- src/pages/home/ui/home.tsx
+++ src/pages/home/ui/home.tsx
@@ -1,25 +1,27 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom'
+
+import '@styles/font.css'
 
 const items = [
-  { title: "부천", locate: "심곡본동", createAt: "2일전" },
-  { title: "부천", locate: "심곡본동", createAt: "2일전" },
-  { title: "부천", locate: "심곡본동", createAt: "2일전" },
-];
+	{ title: '부천', locate: '심곡본동', createAt: '2일전' },
+	{ title: '부천', locate: '심곡본동', createAt: '2일전' },
+	{ title: '부천', locate: '심곡본동', createAt: '2일전' },
+]
 
 export function Home() {
-  return (
-    <div className="w-screen h-screen">
-      Home입니다
-      <Link to="detail">detail로 이동</Link>
-      <ul className="flex flex-col justify-center items-center gap-5">
-        {items.map(({ title, locate, createAt }) => (
-          <li className="">
-            <p>{title}</p>
-            <p>{locate}</p>
-            <p>{createAt}</p>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+	return (
+		<div id="1232" onClick={() => {}} className="h-screen w-screen">
+			Home입니다
+			<Link to="detail">detail로 이동</Link>
+			<ul className="flex flex-col items-center justify-center gap-5">
+				{items.map(({ title, locate, createAt }) => (
+					<li className="">
+						<p>{title}</p>
+						<p>{locate}</p>
+						<p>{createAt}</p>
+					</li>
+				))}
+			</ul>
+		</div>
+	)
 }
